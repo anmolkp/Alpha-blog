318508aeff6768ea6d68387941773a75b5319bec
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-05-25 11:24:07 +0530

    Create a template , action and route

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524182312) do
+ActiveRecord::Schema.define(version: 20160524183123) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
