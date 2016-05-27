32bf5c232bb9d48c5da4cca0013c593d222bd86d
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-05-27 13:22:27 +0530

    Create users model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524183123) do
+ActiveRecord::Schema.define(version: 20160527073958) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160524183123) do
   end
 
   create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
