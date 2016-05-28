e321ce9795499fa04b0b1a15af8d4765f15ccc53
Anmol Khandeparkar <anmolkhandeparkar.nmims@gmail.com>
2016-05-28 18:35:37 +0530

    Add all the user listing and update the show articles for each user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ before_action :set_article, only: [:edit , :update , :show , :destroy]
 
   def create
     @article = Article.new(article_params)
-      @article.user = User.first
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article was successfully created !"
       redirect_to article_path(@article)
